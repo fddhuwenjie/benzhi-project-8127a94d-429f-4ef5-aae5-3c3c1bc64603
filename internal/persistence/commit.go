f2080f5f906e6e22cf7ab38dde02d6067f7ffc48
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Green result for 8127a94d-429f-4ef5-aae5-3c3c1bc64603-bug-2

--- internal/persistence/commit.go
+++ internal/persistence/commit.go
@@ -66,23 +66,11 @@ func (s *Store) Commit(change Commit) error {
 	if err != nil {
 		return err
 	}
-	// 事件先落盘，恢复时能明确识别快照落后；快照和响应均使用原子替换。
-	eventFile, err := os.OpenFile(s.eventPath(change.Case.CaseID), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o640)
-	if err != nil {
-		return err
-	}
-	if _, err = eventFile.Write(eventContent); err == nil {
-		err = eventFile.Sync()
-	}
-	closeErr := eventFile.Close()
-	if err != nil {
-		return err
-	}
-	if closeErr != nil {
-		return closeErr
-	}
+	// 幂等检查和不可变清单存在性检查必须在事件落盘之前完成，
+	// 否则失败请求的事件会残留并导致快照修订与事件数不一致。
+	var manifestContent []byte
 	if change.Manifest != nil {
-		manifestContent, err := json.MarshalIndent(change.Manifest, "", "  ")
+		manifestContent, err = json.MarshalIndent(change.Manifest, "", "  ")
 		if err != nil {
 			return err
 		}
@@ -91,14 +79,75 @@ func (s *Store) Commit(change Commit) error {
 		} else if !errors.Is(statErr, os.ErrNotExist) {
 			return statErr
 		}
-		if err := atomicWrite(s.manifestPath(change.Case.CaseID), append(manifestContent, '\n'), 0o440); err != nil {
+	}
+	// 保存调用前状态，任何后续写入失败都要回滚，使快照修订、事件数量、
+	// 公开清单和幂等响应保持调用前状态。
+	eventPath := s.eventPath(change.Case.CaseID)
+	casePath := s.casePath(change.Case.CaseID)
+	manifestPath := s.manifestPath(change.Case.CaseID)
+	idempotencyPath := s.idempotencyPath(change.Case.CaseID, change.Idempotency.RequestID)
+	preEventStat, _ := os.Stat(eventPath)
+	preCaseStat, _ := os.Stat(casePath)
+	preCaseContent := []byte(nil)
+	if preCaseStat != nil {
+		if existing, readErr := os.ReadFile(casePath); readErr == nil {
+			preCaseContent = existing
+		}
+	}
+	rollback := func(writtenManifest, writtenCase bool) {
+		// 回滚事件帧至调用前状态。
+		if preEventStat == nil {
+			_ = os.Remove(eventPath)
+		} else {
+			_ = os.Truncate(eventPath, preEventStat.Size())
+		}
+		// 回滚快照至调用前状态。
+		if writtenCase {
+			if preCaseStat == nil {
+				_ = os.Remove(casePath)
+			} else {
+				_ = atomicWrite(casePath, preCaseContent, 0o640)
+			}
+		}
+		// 回滚公开清单。
+		if writtenManifest {
+			_ = os.Remove(manifestPath)
+		}
+		// 幂等响应若已写入则删除，避免残留导致重复请求被误判为幂等冲突。
+		if _, statErr := os.Stat(idempotencyPath); statErr == nil {
+			_ = os.Remove(idempotencyPath)
+		}
+	}
+	// 事件先落盘，恢复时能明确识别快照落后；快照和响应均使用原子替换。
+	eventFile, err := os.OpenFile(eventPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o640)
+	if err != nil {
+		return err
+	}
+	if _, err = eventFile.Write(eventContent); err == nil {
+		err = eventFile.Sync()
+	}
+	closeErr := eventFile.Close()
+	if err != nil {
+		rollback(false, false)
+		return err
+	}
+	if closeErr != nil {
+		rollback(false, false)
+		return closeErr
+	}
+	if change.Manifest != nil {
+		if err := atomicWrite(manifestPath, append(manifestContent, '\n'), 0o440); err != nil {
+			rollback(false, false)
 			return err
 		}
 	}
-	if err := atomicWrite(s.casePath(change.Case.CaseID), caseContent, 0o640); err != nil {
+	if err := atomicWrite(casePath, caseContent, 0o640); err != nil {
+		rollback(change.Manifest != nil, false)
 		return err
 	}
-	if err := atomicWrite(s.idempotencyPath(change.Case.CaseID, change.Idempotency.RequestID), idempotencyContent, 0o640); err != nil {
+	if err := atomicWrite(idempotencyPath, idempotencyContent, 0o640); err != nil {
+		// 幂等响应未写入，回滚事件帧、快照和公开清单，使状态保持调用前状态。
+		rollback(change.Manifest != nil, true)
 		return err
 	}
 	return nil
